8154d3b66681b53ff5d28eedd96cd5837c45f1af
DigitalKnight0 <asadredset@gmail.com>
2022-06-29 19:04:15 +0500

    Refactor to use class

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ closeButton.addEventListener('click', (e) => {
     addBook.classList.add('hidden')
 })
 
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+class Book{
+    constructor(title, author, pages, read){
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
+    }
 }
 
 function addBookToLibrary(title, author, pages, read) {
